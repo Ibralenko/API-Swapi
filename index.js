56521e0e94737cc8875e13ff2ddf028e7e879a37
Julia <ibralenko6724809@gmail.com>
2023-07-26 19:40:21 +0300

    add id in func

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-const numberInput = document.querySelector('#number')
+const numberInput = document.querySelector('#number');
+const info = document.querySelector('.info')
 const peopleUrl = 'https://swapi.dev/api/people';
-const filmsUrl = 'https://swapi.dev/api/films';
-const starshipsUrl = 'https://swapi.dev/api/starships';
-const vehiclesUrl = 'https://swapi.dev/api/vehicles';
+const filmsUrl = 'https://swapi.dev/api/films/';
+const starshipsUrl = 'https://swapi.dev/api/starships/';
+const vehiclesUrl = 'https://swapi.dev/api/vehicles/';
 const speciesUrl = 'https://swapi.dev/api/species'
 const planetsUrl = 'https://swapi.dev/api/planets'
 
-function getSelectValue() {
+function createPage(title, year,){
+  const containerPost = document.createElement('div')
+  containerPost.classList.add('container-post')
+  container.append(containerPost)
+  containerPost.innerHTML =
+  `
+  <h2 class="title">${title}</h2>
+  <p class="text">${year}</p>
+  <p class="text">${year}</p>
+  `
+}
+
+function getSelectValue(id) {
   const selectedValue = document.getElementById("select-list").value;
   if (selectedValue === "People") {
-    fetch(peopleUrl)
+    fetch(peopleUrl + `/${id}`)
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.error(error))
   } if (selectedValue === "Films") {
-    fetch(filmsUrl)
+    fetch(filmsUrl + `/${id}`)
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.error(error))
   } if (selectedValue === "Starships") {
-    fetch(starshipsUrl)
+    fetch(starshipsUrl + `/${id}`)
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.error(error))
   } if (selectedValue === "Vehicles") {
-    fetch(vehiclesUrl)
+    fetch(vehiclesUrl + `/${id}`)
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.error(error))
   } if (selectedValue === "Spacies") {
-    fetch(speciesUrl)
+    fetch(speciesUrl + `/${id}`)
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.error(error))
   } if (selectedValue === "Planets") {
-    fetch(planetsUrl)
+    fetch(planetsUrl + `/${id}`)
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.error(error))
   }
 }
+
