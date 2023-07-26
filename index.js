b06e7b2f846e62e64ff9a9f3649545b68678da35
Julia <ibralenko6724809@gmail.com>
2023-07-26 13:29:08 +0300

    add func getSelectValue

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const numberInput = document.querySelector('#number')
 const peopleUrl = 'https://swapi.dev/api/people';
 const filmsUrl = 'https://swapi.dev/api/films';
 const starshipsUrl = 'https://swapi.dev/api/starships';
@@ -5,32 +6,37 @@ const vehiclesUrl = 'https://swapi.dev/api/vehicles';
 const speciesUrl = 'https://swapi.dev/api/species'
 const planetsUrl = 'https://swapi.dev/api/planets'
 
-fetch(peopleUrl)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(error => console.error(error))
-
-fetch(filmsUrl)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(error => console.error(error))
-
-fetch(starshipsUrl)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(error => console.error(error))
-
-fetch(vehiclesUrl)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(error => console.error(error))
-
-fetch(speciesUrl)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(error => console.error(error))
-
-fetch(planetsUrl)
-.then(response => response.json())
-.then(json => console.log(json))
-.catch(error => console.error(error))
+function getSelectValue() {
+  const selectedValue = document.getElementById("select-list").value;
+  if (selectedValue === "People") {
+    fetch(peopleUrl)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  } if (selectedValue === "Films") {
+    fetch(filmsUrl)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  } if (selectedValue === "Starships") {
+    fetch(starshipsUrl)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  } if (selectedValue === "Vehicles") {
+    fetch(vehiclesUrl)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  } if (selectedValue === "Spacies") {
+    fetch(speciesUrl)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  } if (selectedValue === "Planets") {
+    fetch(planetsUrl)
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error))
+  }
+}
