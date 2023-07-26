3e0b405a1780f0c0fd691894607de28cf331d9b7
Julia <ibralenko6724809@gmail.com>
2023-07-26 13:00:19 +0300

    get data

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-fetch('https://swapi.dev/api/', {
-    method: 'POST',
-    body: JSON.stringify({
-      title: title,
-      body: text,
-    }),
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8'
-    }
-  })
-    .then(response => response.json())
-    .then(json => createPost(json.title, json.body))
-    .catch(error => console.error(error));
+const peopleUrl = 'https://swapi.dev/api/people';
+const filmsUrl = 'https://swapi.dev/api/films/';
+const starshipsUrl = 'https://swapi.dev/api/starships';
+const vehiclesUrl = 'https://swapi.dev/api/vehicles';
+const speciesUrl = 'https://swapi.dev/api/species'
+const planetsUrl = 'https://swapi.dev/api/planets'
+
+fetch(peopleUrl)
+.then(response => response.json)
+.then(json => console.log(json))
+.catch(error => console.error(error))
+
+fetch(filmsUrl)
+.then(response => response.json)
+.then(json => console.log(json))
+.catch(error => console.error(error))
+
+fetch(starshipsUrl)
+.then(response => response.json)
+.then(json => console.log(json))
+.catch(error => console.error(error))
+
+fetch(vehiclesUrl)
+.then(response => response.json)
+.then(json => console.log(json))
+.catch(error => console.error(error))
+
+fetch(speciesUrl)
+.then(response => response.json)
+.then(json => console.log(json))
+.catch(error => console.error(error))
+
+fetch(planetsUrl)
+.then(response => response.json)
+.then(json => console.log(json))
+.catch(error => console.error(error))
