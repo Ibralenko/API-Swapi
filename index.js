78b1695ac9ed4cf987d293058aef21cb17dc450c
Julia <ibralenko6724809@gmail.com>
2023-07-26 20:28:01 +0300

    add functions  that create html

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const numberInput = document.querySelector('#number');
 const info = document.querySelector('.info')
-const peopleUrl = 'https://swapi.dev/api/people';
+const peopleUrl = 'https://swapi.dev/api/people/';
 const filmsUrl = 'https://swapi.dev/api/films/';
 const starshipsUrl = 'https://swapi.dev/api/starships/';
 const vehiclesUrl = 'https://swapi.dev/api/vehicles/';
-const speciesUrl = 'https://swapi.dev/api/species'
-const planetsUrl = 'https://swapi.dev/api/planets'
+const speciesUrl = 'https://swapi.dev/api/species/'
+const planetsUrl = 'https://swapi.dev/api/planets/'
 
 function createPeoplePage(name,birth, gender, mass, eye, hair, skin){
   const containerPost = document.createElement('div')
@@ -23,38 +23,97 @@ function createPeoplePage(name,birth, gender, mass, eye, hair, skin){
   `
 }
 
+function createFilmsPage (title, episode, director, producer){
+  const containerPost = document.createElement('div')
+  containerPost.classList.add('container-post')
+  info.append(containerPost)
+  containerPost.innerHTML =
+  `
+  <h2 class="title">${title}</h2>
+  <p class="text">Episode: ${episode}</p>
+  <p class="text">Director: ${director}</p>
+  <p class="text">Producer: ${producer}</p>
+  `
+}
+
+function createStarshpsPage (name, model, manufacturer, length){
+  const containerPost = document.createElement('div')
+  containerPost.classList.add('container-post')
+  info.append(containerPost)
+  containerPost.innerHTML =
+  `
+  <h2 class="title">${name}</h2>
+  <p class="text">Model: ${model}</p>
+  <p class="text">Manufacturer : ${manufacturer}</p>
+  <p class="text">Length: ${length}</p>
+  `
+}
+
+
+function createSpeciesPage (name, classification, designation, language){
+  const containerPost = document.createElement('div')
+  containerPost.classList.add('container-post')
+  info.append(containerPost)
+  containerPost.innerHTML =
+  `
+  <h2 class="title">${name}</h2>
+  <p class="text">Classification: ${classification}</p>
+  <p class="text">Designation : ${designation}</p>
+  <p class="text">Language: ${language}</p>
+  `
+}
+
+function createPlanetsPage (name, diameter, population, climate ){
+  const containerPost = document.createElement('div')
+  containerPost.classList.add('container-post')
+  info.append(containerPost)
+  containerPost.innerHTML =
+  `
+  <h2 class="title">${name}</h2>
+  <p class="text">diameter: ${diameter}</p>
+  <p class="text">population : ${population}</p>
+  <p class="text">climate : ${climate }</p>
+  `
+}
+
+
 function getSelectValue(id) {
   const selectedValue = document.getElementById("select-list").value;
   if (selectedValue === "People") {
-    fetch(peopleUrl + `/${id}`)
+    fetch(peopleUrl + `${id}`)
       .then(response => response.json())
       .then(json => createPeoplePage(json.name,json.birth_year, json.gender, json.mass,json.eye_color,json.hair_color,json.skin_color))
       .catch(error => console.error(error))
+      .finally(console.log('Запрос выполнен'))
   } if (selectedValue === "Films") {
     fetch(filmsUrl + `/${id}`)
       .then(response => response.json())
-      .then(json => createPeoplePage(json.title, json.director, json.species))
+      .then(json => createFilmsPage(json.title, json.episode_id, json.director, json.producer))
       .catch(error => console.error(error))
+      .finally(console.log('Запрос выполнен'))
   } if (selectedValue === "Starships") {
-    fetch(starshipsUrl + `/${id}`)
+    fetch(starshipsUrl + `${id}`)
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json =>  createStarshpsPage(json.name, json.model, json.manufacturer, json.length))
       .catch(error => console.error(error))
+      .finally(console.log('Запрос выполнен'))
   } if (selectedValue === "Vehicles") {
-    fetch(vehiclesUrl + `/${id}`)
+    fetch(vehiclesUrl + `${id}`)
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => createStarshpsPage(json.name, json.model, json.manufacturer, json.length))
       .catch(error => console.error(error))
   } if (selectedValue === "Spacies") {
-    fetch(speciesUrl + `/${id}`)
+    fetch(speciesUrl + `${id}`)
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => createSpeciesPage(json.name, json.classification, json.designation, json.language))
       .catch(error => console.error(error))
+      .finally(console.log('Запрос выполнен'))
   } if (selectedValue === "Planets") {
-    fetch(planetsUrl + `/${id}`)
+    fetch(planetsUrl + `${id}`)
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => createPlanetsPage(json.name, json.diameter, json.population, json.climate))
       .catch(error => console.error(error))
+      .finally(console.log('Запрос выполнен'))
   }
 }
 
